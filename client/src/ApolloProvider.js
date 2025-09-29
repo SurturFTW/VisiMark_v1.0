74692d1df013eb4e6f11204f616efcaac39b8a6f
Pushkar Sane <78395956+SurturFTW@users.noreply.github.com>
2025-09-29 11:11:22 +0530

    Update ApolloProvider API endpoint URL

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -10,7 +10,7 @@ const httpLink = createHttpLink({
   uri:
     !process.env.NODE_ENV || process.env.NODE_ENV === "development"
       ? "http://localhost:4000/"
-      : "visimark-backend.netlify.app",
+      : "https://visimark-v1-0.onrender.com",
 });
 
 const authLink = setContext(() => {
